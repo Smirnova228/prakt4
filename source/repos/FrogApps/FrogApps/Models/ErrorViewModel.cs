f76f1f39fa47ca744513c26abc80fa047d4aede9
Olia <smirnovaoliaqi@gmail.com>
2023-12-10 20:08:31 +0300

    New commit

---
+++ source/repos/FrogApps/FrogApps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FrogApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
